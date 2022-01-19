869463d8d04c1a833e595f8e3e26caf2b6280661
Hiếu Phẩy <hieuphay@pop-os.localdomain>
2022-01-19 14:57:14 +0700

    Some ZotFile tweaks

--- ref-management/.zotero/zotero/uc3o6mvb.default/user.js
+++ ref-management/.zotero/zotero/uc3o6mvb.default/user.js
@@ -1,7 +1,10 @@
 user_pref("extensions.zotero.dataDir", "/home/hieuphay/Dropbox/Documents/Zotero");
 // ZotFile prefereces
-user_pref("extensions.zotfile.authors_delimiter", " ");
+user_pref("extensions.zotfile.authors_delimiter", "-");
 user_pref("extensions.zotfile.dest_dir", "/home/hieuphay/Dropbox/Documents/ZotFile");
+user_pref("extensions.zotfile.import", false);
+user_pref("extensions.zotfile.max_authors", 4);
+user_pref("extensions.zotfile.number_truncate_authors", 4);
 user_pref("extensions.zotfile.removeDiacritics", true);
 user_pref("extensions.zotfile.renameFormat", "{%a}{ (%y)}{ %t}");
 user_pref("extensions.zotfile.source_dir_ff", false);
