43f066394275f1ddce5008a89c51010a05112319
Hiếu Phẩy <hieunguyen31371@gmail.com>
2022-02-18 14:48:21 +0700

    startup.jl: Add function to explore subtypes

--- julia/.julia/config/startup.jl
+++ julia/.julia/config/startup.jl
@@ -2,3 +2,32 @@ using Revise
 using OhMyREPL; colorscheme!("GruvboxDark")
 
 push!(LOAD_PATH, homedir()*"/Dropbox/Codes/Julia")
+
+function Base.show(s::Type)
+    col = isconcretetype(s) ? :yellow : :blue
+    col = isprimitivetype(s) ? :red : col
+    printstyled(string(s); color=col)
+    if ismutable(s)
+        printstyled(" (mutable)"; color=:cyan)
+    end
+end
+
+function subtypetree(roottype, prefix = "")
+    stype = subtypes(roottype)
+    if prefix == ""
+        if length(stype) == 0
+            show(roottype)
+            print(" doesn't have any subtype, and is a subtype of ")
+            show(supertype(roottype))
+        else
+            show(roottype)
+        end
+    end
+    for i in 1:length(stype)
+        print("\n")
+        print(prefix, i == length(stype) ? "└── " : "├── ")
+        s = stype[i]
+        show(s)
+        subtypetree(s, "│   " * prefix)
+    end
+end
