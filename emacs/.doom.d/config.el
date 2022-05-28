2d396b62b1304022c690e99d1361a19031820326
Hiếu Phẩy <hieunguyen31371@gmail.com>
2022-05-29 02:24:41 +0700

    Remove `keycast-mode` configs

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -1153,25 +1153,6 @@ If nil it defaults to `split-string-default-separators', normally
     (setq-local lsp-enable-folding t
                 lsp-folding-range-limit 100)))
 
-(use-package! keycast
-  :commands keycast-mode
-  :config
-  (define-minor-mode keycast-mode
-    "Show current command and its key binding in the mode line."
-    :global t
-    (if keycast-mode
-        (progn
-          (add-hook 'pre-command-hook 'keycast--update t)
-          (add-to-list 'global-mode-string '("" mode-line-keycast " ")))
-      (remove-hook 'pre-command-hook 'keycast--update)
-      (setq global-mode-string (remove '("" mode-line-keycast " ") global-mode-string))))
-  (custom-set-faces!
-    '(keycast-command :inherit doom-modeline-debug
-                      :height 0.9)
-    '(keycast-key :inherit custom-modified
-                  :height 1.1
-                  :weight bold)))
-
 (use-package! org-gcal
   :commands org-gcal-fetch
   :config
--- emacs/.doom.d/packages.el
+++ emacs/.doom.d/packages.el
@@ -4,7 +4,6 @@
 (package! visual-fill-column)
 (package! lsp-treemacs)
 (package! org-super-agenda)
-(package! keycast)
 (package! benchmark-init)
 
 (package! tree-sitter)
