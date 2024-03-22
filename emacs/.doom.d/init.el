0dd3e9e8759ae11759464bcfc302ed772e38288f
Nguyễn Đức Hiếu <hieunguyen31371@gmail.com>
2024-03-21 21:06:52 -0500

    Use the builtin `corfu` module
    It was recently merged to doom-emacs master.

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -22,7 +22,7 @@
 
        :completion
        ;; (company +childframe)           ; the ultimate code completion backend
-       (corfu-LuigiPiucco +icons +orderless)
+       (corfu +icons +orderless)
        ;;helm                          ; the *other* search engine for love and life
        ;;ido                           ; the other *other* search engine...
        ;; (ivy)                        ; a search engine for love and life
